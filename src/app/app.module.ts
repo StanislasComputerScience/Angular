d597946a3cd994a41a04466a83bbaea5a9d73edf
Rémi LABONNE <remilabonne@yahoo.fr>
2024-05-23 16:15:51 +0200

    toto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BlockComponent } from './block/block.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlockComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
